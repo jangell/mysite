458a395aa6cdceb59023f3bc63629bd7ca1b14a7
Jason Angell <jpa322@nyu.edu>
2017-10-26 11:40:49 -0400

    added forms file

--- checkin/models.py
+++ checkin/models.py
@@ -4,3 +4,12 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Checkin(models.Model):
+	timestamp = models.DateField()
+	message = models.TextField(max_length=1000,blank=True,null=True)
+	location = models.CharField(max_length=1000,blank=True,null=True)
+	lat = models.FloatField(blank=True,null=True)
+	lon = models.FloatField(blank=True,null=True)
+	image = models.ImageField(upload_to='checkin_images',blank=True,null=True)
+	def __str__(self):
+		return str(self.timestamp)+' '+str(self.message)
--- checkin/views.py
+++ checkin/views.py
@@ -4,12 +4,43 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from .models import *
 
+# form submission class
+
+
 # Create your views here.
 lm = "on a plane to seattle. window seat. hella good views. let's try a much lemmmmmmgthier message and see how that goes. hey hey hella hella hella hella hella hella hella hella hella hella  goes. hey hey hella hella hella hella hella hella hella hella hella hella"
 sm = "on a plane to seattle"
 def index(request):
 	context = {'time':'literally now','message':lm}
 	return render(request,'checkin/index.html',context)
+
+def contact(request):
+    form_class = ContactForm
+
+    if request.method == 'POST':
+        form = form_class(data=request.POST)
+
+        #from django.core.mail import send_mail
+        #send_mail('Subject here', 'Here is the message.', 'from@example.com', ['to@example.com'], fail_silently=False)
+
+
+        if form.is_valid():
+            contact_name = request.POST.get('contact_name', '')
+            contact_email = request.POST.get('contact_email', '')
+            form_content = request.POST.get('email_content', '')
+            # Email the profile with the contact information
+            template = get_template('contact_template.txt')
+            context = {
+                'contact_name': contact_name,
+                'contact_email': contact_email,
+                'form_content': form_content,
+            }
+            content = template.render(context)
+
+            send_mail('New message from '+contact_name, content, contact_email, ['kcrcranes@gmail.com'])
+            #email.send()
+            messages.success(request, 'Your message has been sent.')
+            return redirect('kcr_cranes:contact')
 
 def submit(request):
 	if request.user.is_superuser:
