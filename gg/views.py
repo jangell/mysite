2a8a2639a1e95d35e0aab0ad923f6d8e3ea8ae4e
Jason Angell <jpa322@nyu.edu>
2018-04-30 15:20:37 -0400

    updated templates to go to IP location, or London center if IP location can't be figured out

--- gg/views.py
+++ gg/views.py
@@ -2,9 +2,30 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render, redirect
-from models import Planting
+from django.contrib.gis.geoip2 import GeoIP2
 
+from models import Planting
 from forms import SubmitForm
+
+# get client ip from a request
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
+# gets the lat and lon from an ip address using the geoip2 library
+def getLatLon(request):
+	try:
+		g = GeoIP2()
+		ip = get_client_ip(request)
+		loc = g.city(ip)
+		return {'lat':loc[0],'lon':loc[1]}
+	# if it doesn't work just use London
+	except Exception as e:
+		return {'lat':51.5074, 'lon':0.1278}
 
 def home(request):
 	return render(request, 'gg/home.html')
@@ -32,10 +53,10 @@ def submit(request):
 		else:
 			print 'invalid form entered'
 			print form.errors
-	context = {'form': form_class}
+	context = {'form': form_class,'location':getLatLon(request)}
 	return render(request, 'gg/submit.html', context)
 
 def explore(request):
 	pp = Planting.objects.all()
-	context = {'plantings': pp}
+	context = {'plantings': pp, 'location':getLatLon(request)}
 	return render(request, 'gg/explore.html', context)
