ca196da651e01cd7b5bf71f84470acee46b500dd
Jason Angell <jpa322@nyu.edu>
2018-11-19 23:40:03 +0100

    added security

--- mysite/settings.py
+++ mysite/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    #'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -130,6 +130,8 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+SECURE_SSL_REDIRECT = True
 
 # analytics
 #PIWIK_DOMAIN_PATH = 'piwik.jangell.org/'
