8b5cbc51fa2e0c38ca4ff7b17e42e260429c6840
jangell <waterboy94@gmail.com>
2024-04-03 10:31:03 -0700

    wip

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^hook_test$', views.hook_test, name='hook_test')
+    url(r'^*$', views.hook_test, name='hook_test')
 ]
